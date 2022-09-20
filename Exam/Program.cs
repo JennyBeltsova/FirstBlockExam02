63492c23ce8ff14bf0f98b944e0c2d88877ce8a8
J Name <1@1.com>
2022-09-20 21:00:49 +0300

    Added realisation

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -7,3 +7,34 @@ Console.WriteLine("Исходный массив:");
 string[] baseArray = {"hello", "2", "world", ":-)"};
 Console.WriteLine("[" + string.Join(", ", baseArray) + "]");
 Console.WriteLine();
+int newArrLength = GetNewArrayLength(baseArray, 3);
+string[] newArr = GetFilteredArray(baseArray, 3, newArrLength);
+
+Console.WriteLine("[" + string.Join(", ", newArr) + "]");
+Console.WriteLine();
+
+
+int GetNewArrayLength(string[] baseArray, int len)
+{
+    int nl = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        if (baseArray[i].Length <= len)
+            nl = nl + 1;
+    };
+    return nl;
+};
+
+string[] GetFilteredArray(string[] baseArray, int len, int newArrLen)
+{
+    string[] newArr = new String[newArrLen];
+    for (int i = 0, j = 0; i < baseArray.Length; i++)
+    {
+        if (baseArray[i].Length <= len)
+            {
+                newArr[j] = baseArray[i];
+                j++;
+            }
+    };
+    return newArr;
+};
